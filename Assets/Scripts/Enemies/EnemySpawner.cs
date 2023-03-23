7a247096779e01593da685e722a61fc5fafc9fe3
The Alpha <contact@nabilalsaiad.com>
2023-03-23 16:16:43 +0800

    Created enemy spawning system
    - Added classes "EnemySpawner" and "Pooler"

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+namespace AceInTheHole
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public static EnemySpawner instance;
+
+        [Range(1f, 20f)]
+        [SerializeField] private float borderDistance = 1f;
+        [Range(1, 5)]
+        [SerializeField] private int maxEnemies = 2;
+
+        [SerializeField] private Pooler<Enemy> enemies = new Pooler<Enemy>();
+
+        public static float RightBorder => instance.borderDistance;
+        public static float LeftBorder => -instance.borderDistance;
+
+        public void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Debug.LogError( "Instance already exists", instance );
+            }
+        }
+
+        private void Start()
+        {
+            enemies.Initialise( this.SpawnNewInstance );
+            this.InvokeRepeating( nameof( SpawnEnemy ), 2, 2 );
+        }
+
+        private void OnDestroy()
+        {
+            this.CancelInvoke( nameof( SpawnEnemy ) );
+        }
+
+        private void SpawnEnemy()
+        {
+            IEnumerable<Enemy> livingEnemies = enemies.pool.Where( x => x.gameObject.activeSelf ).ToArray();
+            //Debug.Log( "livingEnemies.Count(): " + livingEnemies.Count() );
+            bool shouldSpawn = livingEnemies.Count() < maxEnemies;
+            //Debug.Log( "shouldSpawn: " + shouldSpawn );
+            if (shouldSpawn)
+            {
+                _ = enemies.Get();
+            }
+        }
+
+        public static bool ShouldFlip(Enemy enemy)
+        {
+            return enemy.IsDirectionRight
+                ? enemy.transform.position.x >= RightBorder
+                : enemy.transform.position.x <= LeftBorder;
+        }
+        private Enemy SpawnNewInstance(Enemy prefab)
+        {
+            return Instantiate( prefab, this.transform.position, Quaternion.identity );
+        }
+
+#if UNITY_EDITOR
+        [Header("Editor only")]
+        [Range(0.5f, 10.0f)]
+        [SerializeField] private float radius = 1.0f;
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere( new Vector2( this.borderDistance, this.transform.position.y ), this.radius );
+            Gizmos.DrawWireSphere( new Vector2( -this.borderDistance, this.transform.position.y ), this.radius );
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Enemies/Pooler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace AceInTheHole
+{
+    [Serializable]
+    public class Pooler<T>
+        where T : MonoBehaviour
+    {
+        public List<T> pool = new List<T>();
+        public T prefab;
+        private Func<T, T> spawnNewInstance;
+
+        public void Initialise(Func<T, T> spawnNewInstance)
+        {
+            this.spawnNewInstance = spawnNewInstance;
+        }
+
+        public T Get()
+        {
+            int count = pool.Count;
+            for (int i = 0; i < count; i++)
+            {
+                if (!pool[i].gameObject.activeSelf)
+                {
+                    pool[i].gameObject.SetActive( true );
+                    return pool[i];
+                }
+            }
+
+            T obj = spawnNewInstance( this.prefab );
+            pool.Add( obj );
+            return obj;
+        }
+    }
+}
